bbae0b1d9780dc451ad4f6fe85a618bd4c48cf58
Sami <sami;achbani@gmail.com>
2025-04-17 13:49:08 +0200

    Ajout de la bdd

--- src/test/java/com/sami/api/ApiApplicationTests.java
+++ src/test/java/com/sami/api/ApiApplicationTests.java
@@ -3,7 +3,12 @@ package com.sami.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:mysql://localhost:3306/autopro",
+		"spring.datasource.username=root",
+		"spring.datasource.password=root",
+		"spring.jpa.hibernate.ddl-auto=update"
+})
 class ApiApplicationTests {
 
 	@Test
